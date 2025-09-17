76a77976e6e7ee316845b128dd8c5ec79fa8f7fd
Adelson Saguate <adelsonsaguate@acsun-mz.com>
2025-09-17 11:01:40 +0200

    Validações, quando não estiver connectado a base de dados

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\QueryException;
 
 class Handler extends ExceptionHandler
 {
@@ -36,6 +37,13 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (QueryException $e, $request) {
+            // Error code 2002 is for "Can't connect to local MySQL server"
+            if (isset($e->errorInfo[1]) && $e->errorInfo[1] == 2002) {
+                return response()->view('errors.database_connection', [], 503);
+            }
         });
     }
 }
